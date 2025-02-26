6699d5fbcc92ba473e05635460cb09d480dfad49
Admin <nkemakolam.martin@gmail.com>
2025-02-26 20:39:58 +0100

    nawa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-import type { Config } from "tailwindcss";
-
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -16,4 +14,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+};
